b1b3babb286411c48f6e11772e0a76bdecb2ca19
Moshiour <moshiour.rahman@ackosoft.com>
2019-06-14 00:37:48 +0600

    Dummy code removed.

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import {json, urlencoded} from 'body-parser';
 import {HOME_CONTROLLER} from './controllers/home';
 
 const app = express();
-app.use(json());
-app.use(urlencoded({extended: true}));
-app.get('/', HOME_CONTROLLER.index);
-app.get('/users', HOME_CONTROLLER.users);
-app.post('/users/create', HOME_CONTROLLER.create);
+app
+  .use(json())
+  .use(urlencoded({extended: true}));
 
-app.listen(3000, () => {
+
+app.get('/', HOME_CONTROLLER.index)
+  .listen(3000, () => {
   console.log('Application listening On Port 3000');
 });
 
--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,27 +1,12 @@
 import {Request, Response} from 'express';
-import User from '../models/user';
-import {usersList} from '../values/dummyData';
-import {appendFileSync, existsSync, mkdirSync, readFileSync} from 'fs';
 
 const FILES_DIR = './files';
 
 /* GET home page. */
 const index = (req: Request, res: Response) => {
-  if (!existsSync(FILES_DIR)) {
-    mkdirSync(FILES_DIR);
-  }
-  appendFileSync('./files/name.txt', 'Holl molly');
-  res.json(readFileSync('./files/name.txt').toString('utf8'));
-};
-const users = (req: Request, res: Response) => res.json(usersList);
-
-const create = (req: Request, res: Response) => {
-  const user = new User(req.body.name, req.body.username, req.body.email);
-  res.json(user);
+  res.send('Hello World!');
 };
 
 export const HOME_CONTROLLER = {
-  index,
-  users,
-  create
+  index
 };
